c1c9cce74a692ab14434c97ac833512048b80bad
katja4erry <106451546+katja4erry@users.noreply.github.com>
2022-07-12 19:03:14 +0200

    add icon current

--- src/app.js
+++ src/app.js
@@ -28,16 +28,26 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  let responseIcon = response.data.weather[0].icon;
+  if (responseIcon === "01d") {
+    srcIcon = "img/sun.png";
+  }
+  if (responseIcon === "04d") {
+    srcIcon = "img/cloudly.png";
+  }
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute("src", `${srcIcon}`);
 }
 
 let apiKey = "827bb2da8bfca5b7c691ac1dc2ac84b8";
-let city = "Paris";
+let city = "London";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
