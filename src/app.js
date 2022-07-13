f007199ec6a5762b82c119c5390c65044bec7592
katja4erry <106451546+katja4erry@users.noreply.github.com>
2022-07-13 10:54:49 +0200

    added the search engine

--- src/app.js
+++ src/app.js
@@ -93,8 +93,20 @@ function displayTemperature(response) {
   iconElement.setAttribute("src", `${srcIcon}`);
 }
 
-let apiKey = "827bb2da8bfca5b7c691ac1dc2ac84b8";
-let city = "Oslo";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "827bb2da8bfca5b7c691ac1dc2ac84b8";
 
-axios.get(apiUrl).then(displayTemperature);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
